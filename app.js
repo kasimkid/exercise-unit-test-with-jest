b1c2739d646b5ccd5ef06fd7cf7d33745966ab3b
kasimkid <107450115+kasimkid@users.noreply.github.com>
2023-07-15 23:06:42 +0000

    test1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const fromEuroToDollar = function (valueEuro)
-{
-    let dolarValue = valueEuro * 1.2
-    return dolarValue;
-} 
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
 
 const fromDollarToYen = function (dolarValue)
 {
@@ -15,6 +17,8 @@ const fromYenToPound = function ()
     let poundValue = Math.round(yenValue * 0.8 / 127.9 );
     return poundValue;
 }
+
+
 
 
 const sum = (a,b) => {
